50e9f22c5ac434c4e9df39ad9796fac431e0f572
Sabrinale <lengocphuongthao.90@gmail.com>
2017-11-07 21:04:54 +0200

    all commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App2 from './src2/index';
+
+export default class App extends React.Component {
+ 
+  render() {
+    
+    return (
+       <App2/>
+    );
+  }
+}
